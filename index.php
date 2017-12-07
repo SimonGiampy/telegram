52f839d26fba0d7e782a8f5191dd39fd8af0e1bf
SimonGiampy <simo.diablo8@gmail.com>
2017-12-08 00:08:39 +0100

    edit bot webhook token

--- index.php
+++ index.php
@@ -148,7 +148,7 @@ function processMessage($message)
 }
 
 
-define('WEBHOOK_URL', 'https://simongiampy-t.herokuapp.com/');
+define('WEBHOOK_URL', 'https://simongiampybot.herokuapp.com/');
 
 if (php_sapi_name() == 'cli') {
     // if run from console, set or delete webhook
@@ -167,24 +167,3 @@ if (!$update) {
 if (isset($update["message"])) {
     processMessage($update["message"]);
 }
-
-
-
-/*
-$mytoken = "487626617:AAFKwEN6EikkZlNveOi7Rb1qdiReOYDQOqg";
-$request = "https://api.telegram.org/bot".$mytoken ."/getupdates";
-
-$content = file_get_contents($request);
-$update = json_decode($content, true);
-
-print_r($update);
-echo "<br />";
-$text = $update["result"][2]["message"]["text"];
-echo $text;
-
-//http://jsonviewer.stack.hu/
-//https://github.com/php-telegram-bot/core#getupdates-installation
-//https://www.youtube.com/watch?v=hJBYojK7DO4
-//https://api.telegram.org/bot487626617:AAFKwEN6EikkZlNveOi7Rb1qdiReOYDQOqg/setWebhook?url=https://boiling-shore-60433.herokuapp.com/
-
-*/
